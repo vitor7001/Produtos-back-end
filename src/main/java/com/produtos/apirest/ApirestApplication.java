33ee0ac272f863e40c0faaab6cabdab248467be3
vitor7001 <vitorgomes7001@gmail.com>
2019-08-27 18:42:33 -0300

    Models e Interface

--- src/main/java/com/produtos/apirest/ApirestApplication.java
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -2,8 +2,10 @@ package com.produtos.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.produtos.apirest.models")
 public class ApirestApplication {
 
 	public static void main(String[] args) {
