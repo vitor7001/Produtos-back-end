381160f7d4eaaf0ea747c03995161cdbfbcdf02a
vitor7001 <vitorgomes7001@gmail.com>
2019-08-29 22:40:06 -0300

    Tentativa Heroku 2

--- src/main/java/com/produtos/apirest/ApirestApplication.java
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -12,4 +12,5 @@ public class ApirestApplication {
 		SpringApplication.run(ApirestApplication.class, args);
 	}
 
+
 }
--- src/main/java/com/produtos/apirest/config/SwaggerConfig.java
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -13,6 +12,9 @@ import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -23,7 +25,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.produtos.apirest"))
-                .paths(PathSelectors.regex("/api.*"))
+                .paths(regex("/api.*"))
                 .build()
                 .apiInfo(metaInfo());
     }
