795fe450381895433434dd89ebed8a3292941ae3
vitor7001 <vitorgomes7001@gmail.com>
2019-09-04 16:42:33 -0300

    Validações NotNull

--- src/main/java/com/produtos/apirest/models/Produto.java
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -3,12 +3,12 @@ package com.produtos.apirest.models;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 //classe que irá gerar a tabela do banco de dados
 @Entity
@@ -18,50 +18,46 @@ public class Produto implements Serializable{
 	
 	private static final long serialVersionUID = 1l;
 	
-	//chave priária sendo gerada automática pelo hibernate
+	//chave primária sendo gerada automática pelo hibernate
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, nullable = false)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
+	@NotNull
 	private String nome;
 	
+	@NotNull
 	private BigDecimal quantidade;
 	
+	@NotNull
 	private BigDecimal valor;
-
+	
+	
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public BigDecimal getQuantidade() {
 		return quantidade;
 	}
-
 	public void setQuantidade(BigDecimal quantidade) {
 		this.quantidade = quantidade;
 	}
-
 	public BigDecimal getValor() {
 		return valor;
 	}
-
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
 	
-
 	
+
 }
